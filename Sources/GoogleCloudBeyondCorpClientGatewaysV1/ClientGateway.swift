8c49f20c26249bd9397bc8eb032ec6667d2083b6
Carlos O'Ryan <coryan@google.com>
2026-08-19 22:21:47 +0000

    chore: update librarian circa 2026-08-18 (#470)
    In this version of librarian we can automatically import some packages
    with a `@_spi()` attribute. That is handy to hide implementation details
    within `GoogleCloudWkt` and `GoogleCloudGax`.

--- Sources/GoogleCloudBeyondCorpClientGatewaysV1/ClientGateway.swift
+++ Sources/GoogleCloudBeyondCorpClientGatewaysV1/ClientGateway.swift
@@ -15,7 +15,7 @@
 // limitations under the License.
 
 import Foundation
-import GoogleCloudWkt
+@_spi(GoogleCloudInternal) import GoogleCloudWkt
 
 /// Message describing ClientGateway object.
 public struct ClientGateway: Codable, Equatable, GoogleCloudWkt._AnyPackable,
--- Sources/GoogleCloudBeyondCorpClientGatewaysV1/ListClientGatewaysRequest.swift
+++ Sources/GoogleCloudBeyondCorpClientGatewaysV1/ListClientGatewaysRequest.swift
@@ -15,7 +15,7 @@
 // limitations under the License.
 
 import Foundation
-import GoogleCloudWkt
+@_spi(GoogleCloudInternal) import GoogleCloudWkt
 
 /// Message for requesting list of ClientGateways.
 public struct ListClientGatewaysRequest: Codable, Equatable, GoogleCloudWkt._AnyPackable,
